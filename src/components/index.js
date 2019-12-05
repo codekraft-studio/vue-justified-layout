ca56fa68e1c2e580f4904988a25ef6162e956d20
Filippo Conti <filippo@codekraft.it>
2019-12-05 22:22:08 +0100

    feat: Added plugin install function

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import VueJustifiedLayout from './JustifiedLayout'
+
+export { VueJustifiedLayout }
+
+export default function install(Vue) {
+  Vue.component('VueJustifiedLayout', VueJustifiedLayout)
+}
